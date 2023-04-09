f1e94129a3f15b093f5d797931392ac6eee2968f
YOS0602 <yoshitaka.t.biz@gmail.com>
2023-04-09 11:43:16 +0900

    WorksDetailのCSSを整え、wireframeに近い見た目とした

--- src/components/works/worksModal.tsx
+++ src/components/works/worksModal.tsx
@@ -16,15 +16,19 @@ export const WorksModal = (props: Props): JSX.Element => {
 
   return (
     <>
-      <div className="bg-c5 z-40">
-        <button className="bg-c2 rounded-full p-2" onClick={props.closeModal}>
-          X
-        </button>
-
-        <p>workId: {props.workId}</p>
-        <h3>{title}</h3>
-        <div>{description}</div>
-        <div>{skills.length > 0 ? skills : 'Skills Undefined...'}</div>
+      <div className="w- rounded bg-c5 z-40">
+        <div className="px-4">
+          <div className="flex justify-between items-center py-2">
+            <h1 className="text-left text-2xl">{title}</h1>
+            <button className="w-12 h-12 bg-c2 rounded-full p-2" onClick={props.closeModal}>
+              X
+            </button>
+          </div>
+          <div className="text-left text-base py-2">{description}</div>
+          <div className="py-2">
+            <SkillsList skills={skills} />
+          </div>
+        </div>
       </div>
       <div
         className="overlay fixed top-0 left-0 w-full h-full bg-c1 opacity-80 z-30"
@@ -44,5 +48,23 @@ function getWorkContent(workId: number): TWorksContents {
       description: <p>No Description... </p>,
       skills: [],
     }
+  )
+}
+
+function SkillsList(props: { skills: string[] }): JSX.Element {
+  if (!props.skills || props.skills.length === 0) {
+    return <p>Skills Undefined...</p>
+  }
+  return (
+    <>
+      <h2 className="text-left text-xl py-2">Used Skills</h2>
+      <div className="flex flex-wrap justify-start space-x-2 py-2">
+        {props.skills.map((s, i) => (
+          <p key={i} className="px-2  bg-c2 rounded-full">
+            {s}
+          </p>
+        ))}
+      </div>
+    </>
   )
 }
