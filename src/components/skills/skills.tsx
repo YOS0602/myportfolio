fe021686f5e6da25ca9a5dd1522d9f4039082548
YOS0602 <yoshitaka.t.biz@gmail.com>
2023-06-09 17:00:20 +0900

    create Skills component and make it display in index.tsx

---
+++ src/components/skills/skills.tsx
@@ -0,0 +1,3 @@
+export const Skills = (): JSX.Element => {
+  return <p>Under Development...</p>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from './_app'
 import { Layout } from '@/components/layout/layout'
 import { Profile } from '@/components/profile/profile'
+import { Skills } from '@/components/skills/skills'
 import { Qualifications } from '@/components/qualifications/qualifications'
 import { Works } from '@/components/works/works'
 import { Contacts } from '@/components/contacts/contacts'
@@ -18,35 +19,50 @@ const Home: NextPageWithLayout = () => {
         </Head>
 
         <div className="my-10 md:my-44">
-          <h1 id="section-profile" className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20">
+          <h1
+            id="section-profile"
+            className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20"
+          >
             Profile
           </h1>
           <Profile />
         </div>
 
         <div className="my-10 md:my-44">
-          <h1 id="section-skills" className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20">
+          <h1
+            id="section-skills"
+            className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20"
+          >
             Skills
           </h1>
-          {/* <Skill /> */}
+          <Skills />
         </div>
 
         <div className="my-10 md:my-44">
-          <h1 id="section-qualifications" className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20">
+          <h1
+            id="section-qualifications"
+            className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20"
+          >
             Qualifications
           </h1>
           <Qualifications />
         </div>
 
         <div className="my-10 md:my-44">
-          <h1 id="section-works" className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20">
+          <h1
+            id="section-works"
+            className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20"
+          >
             Works
           </h1>
           <Works />
         </div>
 
         <div className="my-10 md:my-44">
-          <h1 id="section-contacts" className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20">
+          <h1
+            id="section-contacts"
+            className="text-4xl md:text-6xl pt-20 pb-4 md:pb-20"
+          >
             Contacts
           </h1>
           <Contacts />
