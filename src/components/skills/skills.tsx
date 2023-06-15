4a594df095b24662317dfb382fa03ef8d3ba5be3
YOS0602 <yoshitaka.t.biz@gmail.com>
2023-06-15 17:29:08 +0900

    add bar charts of skills

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,5 +1,29 @@
 import React from 'react'
+import { SkillsCharts } from './skillsCharts'
 
 export const Skills = (): JSX.Element => {
-  return <p>Under Development...</p>
+  return (
+    <>
+      <div>
+        <h3 className="text-xl md:text-2xl pt-10">Frontend</h3>
+        <SkillsCharts skillKind="frontend" />
+      </div>
+      <div>
+        <h3 className="text-xl md:text-2xl pt-10">Server Side</h3>
+        <SkillsCharts skillKind="server side" />
+      </div>
+      <div>
+        <h3 className="text-xl md:text-2xl pt-10">Framework</h3>
+        <SkillsCharts skillKind="framework" />
+      </div>
+      <div>
+        <h3 className="text-xl md:text-2xl pt-10">Cloud</h3>
+        <SkillsCharts skillKind="cloud" />
+      </div>
+      <div>
+        <h3 className="text-xl md:text-2xl pt-10">Other</h3>
+        <SkillsCharts skillKind="other" />
+      </div>
+    </>
+  )
 }
---
+++ src/components/skills/skillsCharts.tsx
@@ -0,0 +1,39 @@
+import { skills, TSkill, TSkillKind } from './skillsData'
+import styles from './skillsCharts.module.css'
+
+function filterSkills(kind: TSkillKind): TSkill[] {
+  return skills.filter((v) => v.kind === kind)
+}
+
+type Props = {
+  skillKind: TSkillKind
+}
+
+export const SkillsCharts = (props: Props): JSX.Element => {
+  return (
+    <>
+      <div className="grid grid-cols-4 xl:grid-cols-7 gap-4 py-4">
+        {filterSkills(props.skillKind).map((v, i) => (
+          <div key={i}>
+            <BarChart v={v} />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+function BarChart({ v }: { v: TSkill }): JSX.Element {
+  return (
+    <>
+      <div className="flex items-end mx-auto my-2 bg-slate-400 w-2 h-28 rounded-full">
+        <div
+          className={`${
+            styles[`chart-bar-level-${v.level}`]
+          } bg-c4 w-2 rounded-full`}
+        ></div>
+      </div>
+      <p className="text-xs md:text-base">{v.name}</p>
+    </>
+  )
+}
