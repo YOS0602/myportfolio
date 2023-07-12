c017033c6dee242193f327bf43bb1e9664ff06ad
YOS0602 <yoshitaka.t.biz@gmail.com>
2023-07-12 20:10:34 +0900

    [Qualification] add 漢検2級 and AZ-900

--- src/components/qualifications/qualificationsTable.tsx
+++ src/components/qualifications/qualificationsTable.tsx
@@ -2,16 +2,22 @@ import styles from './qualificationsTable.module.css'
 
 export const QualificationsTable = (): JSX.Element => {
   const qualificationsData = [
-    { id: 5, ym: '2022/03', q: 'Certified Scrum Developer' },
-    { id: 4, ym: '2021/12', q: 'Certified ScrumMaster' },
-    { id: 3, ym: '2021/03', q: '応用情報技術者' },
-    { id: 2, ym: '2020/09', q: 'TOEIC 825点' },
-    { id: 1, ym: '2019/11', q: '基本情報技術者' },
+    // {
+    //   ym: '2023/07',
+    //   q: 'Microsoft Certified: Azure Developer Associate AZ-204',
+    // },
+    { ym: '2023/07', q: 'Microsoft Certified: Azure Fundamentals AZ-900' },
+    { ym: '2022/06', q: '日本漢字能力検定2級' },
+    { ym: '2022/03', q: 'Certified Scrum Developer' },
+    { ym: '2021/12', q: 'Certified ScrumMaster' },
+    { ym: '2021/03', q: '応用情報技術者' },
+    { ym: '2020/09', q: 'TOEIC 825点' },
+    { ym: '2019/11', q: '基本情報技術者' },
   ]
 
   const makeTbody = () => {
-    return qualificationsData.map((d) => (
-      <tr key={d.id} className={styles.tr}>
+    return qualificationsData.map((d, i) => (
+      <tr key={i} className={styles.tr}>
         <td className={styles.td}>{d.ym}</td>
         <td className={`text-left p-12 ${styles.td}`}>{d.q}</td>
       </tr>
