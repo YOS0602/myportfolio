bf79ca723d88fbfa3223c55d4b0691a3216655a0
YOS0602 <yoshitaka.t.biz@gmail.com>
2023-07-12 20:15:59 +0900

    move qualificationsData to const

--- src/components/qualifications/qualificationsTable.tsx
+++ src/components/qualifications/qualificationsTable.tsx
@@ -1,20 +1,7 @@
 import styles from './qualificationsTable.module.css'
+import { qualificationsData } from '@/const/qualificationsData'
 
 export const QualificationsTable = (): JSX.Element => {
-  const qualificationsData = [
-    // {
-    //   ym: '2023/07',
-    //   q: 'Microsoft Certified: Azure Developer Associate AZ-204',
-    // },
-    { ym: '2023/07', q: 'Microsoft Certified: Azure Fundamentals AZ-900' },
-    { ym: '2022/06', q: '日本漢字能力検定2級' },
-    { ym: '2022/03', q: 'Certified Scrum Developer' },
-    { ym: '2021/12', q: 'Certified ScrumMaster' },
-    { ym: '2021/03', q: '応用情報技術者' },
-    { ym: '2020/09', q: 'TOEIC 825点' },
-    { ym: '2019/11', q: '基本情報技術者' },
-  ]
-
   const makeTbody = () => {
     return qualificationsData.map((d, i) => (
       <tr key={i} className={styles.tr}>
---
+++ src/const/qualificationsData.ts
@@ -0,0 +1,13 @@
+export const qualificationsData = [
+  // {
+  //   ym: '2023/07',
+  //   q: 'Microsoft Certified: Azure Developer Associate AZ-204',
+  // },
+  { ym: '2023/07', q: 'Microsoft Certified: Azure Fundamentals AZ-900' },
+  { ym: '2022/06', q: '日本漢字能力検定2級' },
+  { ym: '2022/03', q: 'Certified Scrum Developer' },
+  { ym: '2021/12', q: 'Certified ScrumMaster' },
+  { ym: '2021/03', q: '応用情報技術者' },
+  { ym: '2020/09', q: 'TOEIC 825点' },
+  { ym: '2019/11', q: '基本情報技術者' },
+] as const
